ce4031f26124e2c13ab312509862a854292d8272
ant <theant@mail.ru>
2023-05-15 20:25:29 +0400

    webview test

--- app/src/main/java/com/test/wvtestapp/MainActivity.kt
+++ app/src/main/java/com/test/wvtestapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.test.wvtestapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.mainWebView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        // Загрузка веб-страницы
+        webView.loadUrl("https://google.com")
+    }
+
+    override fun onBackPressed() {
+        // Проверка, есть ли история переходов назад в WebView
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
