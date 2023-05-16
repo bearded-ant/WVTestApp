979bc2d915896132d7fcd0923a97ff70da95176b
ant <theant@mail.ru>
2023-05-16 11:10:12 +0400

    oneSignal init

--- app/src/main/java/com/test/wvtestapp/base/App.kt
+++ app/src/main/java/com/test/wvtestapp/base/App.kt
@@ -2,12 +2,25 @@ package com.test.wvtestapp.base
 
 import android.app.Application
 import com.appsflyer.AppsFlyerLib
+import com.onesignal.OneSignal
 
 const val AF_DEV_KEY: String = "0"
+const val ONESIGNAL_APP_ID: String = "1"
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         AppsFlyerLib.getInstance().init(AF_DEV_KEY, null, this)
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+        OneSignal.promptForPushNotifications();
     }
 }
